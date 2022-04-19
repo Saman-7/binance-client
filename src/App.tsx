7de9a77226abbe6590a3688b2c81adc5ec1da10b
Saman-7 <saman.explosion.021@gmail.com>
2022-04-19 12:43:49 +0430

    connect to apollo server and get candles data

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
-function App() {
-  return <div></div>;
-}
+import { gql, useQuery } from "@apollo/client";
+
+const GET_CANDLES = gql`
+  {
+    getCandles {
+      candleId
+      pair
+      startTime
+      endTime
+      open
+      high
+      low
+      close
+      interval
+      volume
+      isUpCandle
+    }
+  }
+`;
+
+const App = () => {
+  const { loading, error, data } = useQuery(GET_CANDLES);
+
+  if (loading) return <h1>Loading ...</h1>;
+  if (error) return <h1>Error :/</h1>;
+  return <div>{JSON.stringify(data)}</div>;
+};
 
 export default App;
