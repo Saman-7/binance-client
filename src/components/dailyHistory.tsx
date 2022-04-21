0a92c7f0a34181817c0037f03dd0cbef346e4fd1
saman <saman.explosion.021@gmail.com>
2022-04-21 22:17:50 +0430

    refactor daily history header

--- src/components/dailyHistory.tsx
+++ src/components/dailyHistory.tsx
@@ -1,20 +1,19 @@
 import styled from "styled-components";
-import { ReactComponent as Search } from "../svg/search.svg";
 import { ReactComponent as Play } from "../svg/play.svg";
 import { ReactComponent as Notebook } from "../svg/notebook.svg";
 
 const DailyHostoryContainer = styled.div`
   grid-area: daily;
-  height: 50px;
   padding: 10px 0px;
   display: flex;
   justify-content: space-around;
   align-items: center;
   background-color: #161a1e;
   color: white;
+  border-bottom: 1px #3d3d3d solid;
 
   & h1 {
-    margin: 0px;
+    margin: 0px 25px 0px 0px;
     font-size: 20px;
     font-weight: 500;
   }
@@ -27,13 +26,13 @@ const DailyHostoryContainer = styled.div`
 
   & div .price {
     margin: 0px;
-    font-size: 18px;
+    font-size: 16px;
     font-weight: 500;
   }
 
   & div p {
     margin: 5px 0px;
-    font-size: 14px;
+    font-size: 12px;
   }
 
   & div .head {
@@ -44,7 +43,7 @@ const DailyHostoryContainer = styled.div`
 const DailyHistory = () => {
   return (
     <DailyHostoryContainer>
-      <div>
+      <div style={{ borderRight: "1px #3d3d3d solid" }}>
         <h1>BTC/USDT</h1>
         <div style={{ display: "inline-flex", alignItems: "center" }}>
           <Notebook style={{ width: "1.2em", color: "#848e9c" }} />
@@ -75,29 +74,10 @@ const DailyHistory = () => {
         <p className="head">24h Volume(USDT)</p>
         <p>1,442,216,580.47</p>
       </div>
-      <div style={{ display: "inline-flex" }}>
+      <div style={{ width: "20%" }} />
+      <div style={{ display: "inline-flex", color: "#848e9c" }}>
         <Play style={{ width: "1.2em", marginRight: "5px" }} />
         <p>Spot Tutorial</p>
-      </div>
-      <div
-        style={{
-          height: "24px",
-          display: "inline-flex",
-          alignItems: "center",
-          backgroundColor: "#2B3139",
-          borderRadius: "5px",
-        }}
-      >
-        <Search style={{ width: "1.3em" }} />
-        <input
-          style={{
-            backgroundColor: "inherit",
-            color: "white",
-            border: "none",
-            outline: "none",
-          }}
-          placeholder="Search"
-        />
       </div>
     </DailyHostoryContainer>
   );
