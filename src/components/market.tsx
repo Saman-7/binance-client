ea9bd9e6e1163b71edaaee0871f347f423ab33f2
saman <saman.explosion.021@gmail.com>
2022-04-25 19:33:27 +0430

    create market activities

--- src/components/market.tsx
+++ src/components/market.tsx
@@ -3,6 +3,8 @@ import { ReactComponent as SearchLogo } from "../svg/search.svg";
 import { ReactComponent as StarLogo } from "../svg/star.svg";
 import { ReactComponent as LessThatSign } from "../svg/lessThatSign.svg";
 import { randomNumber } from "../utils/randomNumber";
+import { ReactComponent as DoubleArrowTop } from "../svg/doubleArrowTop.svg";
+import { ReactComponent as ArrowUp } from "../svg/arrowUp.svg";
 
 const MarketContainer = styled.div`
   grid-area: market;
@@ -106,6 +108,38 @@ const TradesContainer = styled.div`
   }
 `;
 
+const MarketActivities = styled.div`
+  & div {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px 15px;
+  }
+  & div span {
+    font-weight: 600;
+    color: white;
+  }
+`;
+
+const ItemActivities = styled.tr`
+  font-size: 12px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: #848e9c;
+  border-bottom: 1px #252930 solid;
+  padding: 3px 12px;
+
+  & td {
+    display: flex;
+    flex-direction: column;
+  }
+
+  & td span {
+    margin-bottom: 3px;
+  }
+`;
+
 const CurrencyRandomItem = () => {
   const pair = "BNB";
   const price = Math.random().toFixed(5);
@@ -200,6 +234,62 @@ const Market = () => {
           ))}
         </tbody>
       </TableContainer>
+
+      <MarketActivities>
+        <div>
+          <span>Market Activities</span>
+          <DoubleArrowTop
+            style={{
+              width: "25px",
+              transform: "rotate(180deg)",
+              color: "#5E6673",
+            }}
+          />
+        </div>
+
+        <table style={{ width: "100%" }}>
+          <ItemActivities>
+            <td>
+              <span>ORN/USDT</span>
+              <span>16:28:06</span>
+            </td>
+            <td>
+              <span>-6.88%</span>
+              <span>New 24h Low</span>
+            </td>
+            <td>
+              <ArrowUp
+                style={{
+                  width: "45px",
+                  backgroundColor: "#15342D",
+                  color: "#0ecb81",
+                  borderRadius: "4px",
+                }}
+              />
+            </td>
+          </ItemActivities>
+          <ItemActivities>
+            <td>
+              <span>ORN/USDT</span>
+              <span>16:28:06</span>
+            </td>
+            <td>
+              <span>-6.88%</span>
+              <span>New 24h Low</span>
+            </td>
+            <td>
+              <ArrowUp
+                style={{
+                  width: "45px",
+                  backgroundColor: "#15342D",
+                  color: "#0ecb81",
+                  borderRadius: "4px",
+                }}
+              />
+            </td>
+          </ItemActivities>
+        </table>
+      </MarketActivities>
     </MarketContainer>
   );
 };
