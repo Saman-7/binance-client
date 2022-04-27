39e6412b9f12528b18eb22ed5ae7332fb4470769
Saman-7 <saman.explosion.021@gmail.com>
2022-04-27 11:52:03 +0430

    add suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 import "./App.css";
-import { VFC } from "react";
-import Header from "./components/header";
-import DailyHistory from "./components/dailyHistory";
-import OrderList from "./components/orderList";
+import { Suspense, VFC } from "react";
+import Header from "./components/Header";
+import DailyHistory from "./components/DailyHistory";
+import OrderList from "./components/OrderList";
 import Chart from "./components/useChart";
-import Market from "./components/market";
-import Details from "./components/details";
-import Footer from "./components/footer";
+import Market from "./components/Market";
+import Details from "./components/Details";
+import Footer from "./components/Footer";
 
 const App: VFC = () => {
   //
   return (
     <div className="app">
-      <Header />
-      <DailyHistory />
-      <OrderList />
-      <Chart />
-      <Market />
-      <Details />
-      <Footer />
+      <Suspense fallback={<h1>Loading ...</h1>}>
+        <Header />
+        <DailyHistory />
+        <OrderList />
+        <Chart />
+        <Market />
+        <Details />
+        <Footer />
+      </Suspense>
     </div>
   );
 };
