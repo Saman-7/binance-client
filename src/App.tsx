15ada2a9f24d40dedf0b8a788ba68c0504e7800d
Saman-7 <saman.explosion.021@gmail.com>
2022-04-30 16:28:51 +0430

    complete page suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import "./App.css";
-import { Suspense, VFC } from "react";
-import Header from "./components/Header";
-import DailyHistory from "./components/DailyHistory";
-import OrderList from "./components/useOrderList";
-import Chart from "./components/useChart";
-import Market from "./components/Market";
-import Details from "./components/Details";
-import Footer from "./components/Footer";
+import { lazy, Suspense, VFC } from "react";
+import Loading from "./components/Loading";
+const Header = lazy(() => import("./components/Header"));
+const DailyHistory = lazy(() => import("./components/DailyHistory"));
+const OrderList = lazy(() => import("./components/useOrderList"));
+const Chart = lazy(() => import("./components/useChart"));
+const Market = lazy(() => import("./components/Market"));
+const Details = lazy(() => import("./components/Details"));
+const Footer = lazy(() => import("./components/Footer"));
 
 const App: VFC = () => {
   //
   return (
     <div className="app">
-      <Suspense fallback={<h1>Loading ...</h1>}>
+      <Suspense fallback={<Loading />}>
         <Header />
         <DailyHistory />
         <OrderList />
