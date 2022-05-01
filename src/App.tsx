46a3384b461fa58e4588da702344e46e96818242
Saman-7 <saman.explosion.021@gmail.com>
2022-05-01 15:45:21 +0430

    sort filenames

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import "./App.css";
 import { FC, lazy, Suspense } from "react";
 import Loading from "./components/Loading";
 const Header = lazy(() => import("./components/Header"));
-const DailyHistory = lazy(() => import("./components/DailyHistory"));
+const DailyHistory = lazy(() => import("./components/useDailyHistory"));
 const OrderList = lazy(() => import("./components/useOrderList"));
 const Chart = lazy(() => import("./components/useChart"));
-const Market = lazy(() => import("./components/Market"));
+const Market = lazy(() => import("./components/MarketTrades"));
 const Details = lazy(() => import("./components/Details"));
 const Footer = lazy(() => import("./components/Footer"));
 
