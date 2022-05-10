7b16bf066fdb71640147cdd96c940785405da786
saman <saman.explosion.021@gmail.com>
2022-05-10 17:37:17 +0430

    Make daily history responsive for mobile

--- src/components/DailyHistory.tsx
+++ src/components/DailyHistory.tsx
@@ -2,12 +2,15 @@ import { FC, useEffect, useState } from "react";
 import { ReactComponent as Play } from "../svg/play.svg";
 import { ReactComponent as Notebook } from "../svg/notebook.svg";
 import {
-  DailyHostoryContainer,
+  DailyHostoryContainer as DailyHistoryContainer,
+  LeftColumn,
+  RightColumn,
   SectionDaily,
 } from "../style/DailyHistoryStyled";
 import { randomNumber } from "../utils/RandomNumber";
 import { randomColor } from "../utils/RandomColor";
 import { useIsDesktop } from "../utils/useIsDesktop";
+import { useIsMobile } from "../utils/useIsMobile";
 
 const DailyHistory: FC = () => {
   const [price, setPrice] = useState({
@@ -16,6 +19,7 @@ const DailyHistory: FC = () => {
   });
 
   const isDesktop = useIsDesktop();
+  const isMobile = useIsMobile();
 
   useEffect(() => {
     const id = setInterval(() => {
@@ -27,52 +31,65 @@ const DailyHistory: FC = () => {
   }, [price]);
 
   return (
-    <DailyHostoryContainer>
+    <DailyHistoryContainer>
       <SectionDaily className="left">
-        <div style={{ borderRight: "1px #252930 solid" }}>
-          <h1>BTC/USDT</h1>
-          <div style={{ display: "flex", alignItems: "center" }}>
-            <Notebook style={{ width: "1.2em", color: "#848e9c" }} />
-            <p className="bitcoin-price">Bitcoin Price</p>
+        <LeftColumn>
+          <div style={{ borderRight: "1px #252930 solid" }}>
+            {!isMobile ? (
+              <h1>BTC/USDT</h1>
+            ) : (
+              <>
+                <h1>BTC</h1>
+                <h1>/USDT</h1>
+              </>
+            )}
+
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <Notebook style={{ width: "1.2em", color: "#848e9c" }} />
+              <p className="bitcoin-price">Bitcoin Price</p>
+            </div>
           </div>
-        </div>
-        <div>
-          <p className="price" style={{ color: price.color }}>
-            {price.value}.00
-          </p>
-          <p>${price.value}.00</p>
-        </div>
-        <div>
-          <p className="head">24h Change</p>
-          <p className="red">737.26 +1.81%</p>
-        </div>
-        <div>
-          <p className="head">24h High</p>
-          <p>41,760.00</p>
-        </div>
-        <div>
-          <p className="head">24h Low</p>
-          <p>40,571.00</p>
-        </div>
-        <div>
-          <p className="head">24h Volume(BTC)</p>
-          <p>34,941.65</p>
-        </div>
-        <div>
-          <p className="head">24h Volume(USDT)</p>
-          <p>1,442,216,580.47</p>
-        </div>
+          <div>
+            <p className="price" style={{ color: price.color }}>
+              {price.value}.00
+            </p>
+            {!isMobile && <p>${price.value}.00</p>}
+          </div>
+          <div>
+            {!isMobile && <p className="head">24h Change</p>}
+            <p className="red">737.26 +1.81%</p>
+          </div>
+        </LeftColumn>
+
+        <RightColumn>
+          <div>
+            <p className="head">24h High</p>
+            <p>41,760.00</p>
+          </div>
+          <div>
+            <p className="head">24h Low</p>
+            <p>40,571.00</p>
+          </div>
+          <div>
+            <p className="head">24h Volume(BTC)</p>
+            <p>34,941.65</p>
+          </div>
+          <div>
+            <p className="head">24h Volume(USDT)</p>
+            <p>1,442,216,580.47</p>
+          </div>
+        </RightColumn>
       </SectionDaily>
 
-      {!isDesktop ? (
+      {!isDesktop && (
         <SectionDaily className="right">
           <div>
             <Play style={{ width: "1.2em", marginRight: "5px" }} />
-            <p>Spot Tutorial</p>
+            <span>Spot Tutorial</span>
           </div>
         </SectionDaily>
-      ) : null}
-    </DailyHostoryContainer>
+      )}
+    </DailyHistoryContainer>
   );
 };
 
--- src/style/DailyHistoryStyled.tsx
+++ src/style/DailyHistoryStyled.tsx
@@ -14,39 +14,23 @@ export const DailyHostoryContainer = styled.div`
     margin: 5px 0px;
     font-size: 12px;
   }
+
+  @media (max-width: 767px) {
+    border-bottom: 7px #14151a solid;
+  }
 `;
 
 export const SectionDaily = styled.div`
   &.left {
     display: flex;
-    justify-content: space-around;
+    justify-content: space-between;
     align-items: center;
 
-    & div {
+    & div div {
       margin: 0px 13px;
-      & .red {
-        color: #f6465d;
+      & .head {
+        color: #848e9c;
       }
-    }
-
-    & h1 {
-      margin: 0px 15px;
-      font-size: 20px;
-      font-weight: 500;
-    }
-    & div .bitcoin-price {
-      text-decoration: underline;
-      font-size: 13px;
-      color: #848e9c;
-    }
-    & div .price {
-      margin: 0px;
-      font-size: 16px;
-      font-weight: 500;
-      color: #0ecb81;
-    }
-    & div .head {
-      color: #848e9c;
     }
   }
 
@@ -56,6 +40,65 @@ export const SectionDaily = styled.div`
     & div {
       display: flex;
       color: #848e9c;
+      font-size: 13px;
+    }
+  }
+
+  @media (max-width: 767px) {
+    width: 100%;
+  }
+`;
+
+export const LeftColumn = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+
+  & h1 {
+    margin: 0px 15px;
+    font-size: 20px;
+    font-weight: 500;
+  }
+
+  & .bitcoin-price {
+    text-decoration: underline;
+    font-size: 13px;
+    color: #848e9c;
+  }
+
+  & div .price {
+    margin: 0px;
+    font-size: 16px;
+    font-weight: 500;
+    color: #0ecb81;
+  }
+
+  & .red {
+    color: #f6465d;
+  }
+
+  @media (max-width: 767px) {
+    flex-direction: column;
+
+    & div .price {
+      font-size: 24px;
+    }
+  }
+`;
+
+export const RightColumn = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+
+  @media (max-width: 767px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: repeat(2, 1fr);
+    grid-gap: 15px;
+
+    & div {
+      padding-right: 70px;
     }
   }
 `;
